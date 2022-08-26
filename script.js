b1a19b4c9bf7f1e6ace4d2d4dd672a9a2ed5dc55
Iago M. Souza <iago.thedeck@gmail.com>
2022-08-26 08:46:27 -0300

    enviando msg e utilizando tecla enter

--- script.js
+++ script.js
@@ -51,7 +51,86 @@ function handleError (error) {
 
 setInterval(keepLoggedIn, 5000);
 
+/* até aqui é pra entrar no bate papo */
+function printMessages (answer) {
+    let msg = [];
+    msg = answer.data;
 
+    let content = document.querySelector(".msg-box");
+    
+    content.innerHTML = "";
 
+    for(let i = 0 ; i < msg.length ; i++) {
+        if(msg[i].type === "status"){
+            content.innerHTML += `
+                <div class="${msg[i].type}">
+                    <span class="time">(${msg[i].time})</span>
+                    <span class="text"><strong>${msg[i].from}</strong> ${msg[i].text}
+                </div>
+                `;
+        }
+        
+        if(msg[i].type === "message"){
+            content.innerHTML += `
+                <div class="${msg[i].type}">
+                    <span class="time">(${msg[i].time})</span>
+                    <span class="text"><strong>${msg[i].from}</strong> para <strong>${msg[i].to}</strong>: ${msg[i].text}
+                </div>
+                `;
+        }
+
+        if(msg[i].type === "private_message" && (msg[i].to === userName.name || msg[i].from === userName.name)){
+            content.innerHTML += `
+                <div class="${msg[i].type}">
+                    <span class="time">(${msg[i].time})</span>
+                    <span class="text"><strong>${msg[i].from}</strong> reservadamente para <strong>${msg[i].to}</strong>: ${msg[i].text}
+                </div>
+                `;
+        }
+    }
+    
+    let lastMessage = document.querySelector(".msg-box").lastElementChild;
+    lastMessage.scrollIntoView();
+
+}
+
+setInterval(searchMessages, 3000);
+
+let onSend = () => {
+    searchMessages();
+
+    document.querySelector(".type-here").value = "";
+}
+
+function sendMessage () {
+    const text = document.querySelector(".type-here").value;
+    const msg = {
+        from: userName.name,
+        to: to,
+        text: text,
+        type: type
+    };
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
+
+    promise.then(onSend);
+    promise.catch(window.location.reload);
+}
+
+document.addEventListener("keypress", function(e) {
+    if(e.key === "Enter") {
+        let button;
+        if(document.querySelector(".start-screen").classList.contains("hidden")) {
+            button = document.querySelector(".btn");
+        }
+
+        if(document.querySelector(".chat").classList.contains("hidden")) {
+            button = document.querySelector(".enter");
+        }
+        button.click();
+    }
+ });
+
+ 
      
  
